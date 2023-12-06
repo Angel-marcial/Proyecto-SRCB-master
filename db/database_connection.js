1e61544f3ecef7aedd8ad493b1576ad76bb5ed56
Angel Geovanni Marcial Morales <Skatepanuco@gmail.com>
2023-12-06 02:52:19 -0600

    sd

--- db/database_connection.js
+++ db/database_connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost/3308',
+  host: '127.0.0.1',
   user: 'root',
   password: '',
   database: 'SALAS_DB',
-});
+})
 
 module.exports = pool;
 
